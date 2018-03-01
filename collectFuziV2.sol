54ac1776c0e7023b32206b6fe7f33c063b96ea2c
difengJ <361833008@qq.com>
2018-03-01 15:48:31 +0800

    Update collectFuziV2.sol

--- collectFuziV2.sol
+++ collectFuziV2.sol
@@ -6,51 +6,44 @@ contract collectFuzi{
      * The start and end dates respectively convert to the following
      * timestamps:
      *  START_DATE  => 03/01/2018 @ 12:00am (UTC)
-     *  END_DATE    => 03/31/2018 @ 12:00am (UTC)
+     *  END_DATE    => 03/15/2018 @ 12:00am (UTC)
      */
     uint256 constant START_DATE = 1519862400;
     uint256 constant END_DATE = 1522454400;
     /*
      * The amount of seconds within a single game.
      *
-     * one hour per game:
-     *      60 x 60 = 3600
+     * hours per game:
+     *        60 x 60 = 3600
      */
-    uint16 constant GAME_DURATION_IN_SECONDS = 3600;
-    
+    uint16 constant GAME_DURATION_IN_SECONDS = 43200;
+    uint8 timeSlotCount = uint8((END_DATE - START_DATE) / GAME_DURATION_IN_SECONDS);
     uint public types;
     uint public maxperType;
     address public owner;
-    mapping (address => mapping(uint => uint))  fuZiof;
-    mapping (uint => uint) fuZiRemaining;
     struct epoch_data {
         mapping (address => mapping(uint => uint))  fuZiof;
         mapping (uint => uint) fuZiRemaining;
     }
     
-    epoch_data [] epoch_data_all;
+    epoch_data [720] epoch_data_all;
     
     event recieveFuZi(uint);
-    
     modifier onlyOwner{
         require(msg.sender == owner);
         _;
-    }
-
-    function TotalEpoch() public returns(uint) {
-        uint256 secondsInTotalPeriod = END_DATE - START_DATE;
-        uint8 timeSlotCount = uint8(secondsInTotalPeriod / GAME_DURATION_IN_SECONDS);
-        return (timeSlotCount);
     }
 
     function collectFuzi(uint _types,uint _maxperType) payable{
         owner = msg.sender;
         if(_types != 0){types = _types;}
         if(_maxperType != 0){maxperType = _maxperType;}
-        for (uint i = 0; i < _types; i++) {
-            fuZiRemaining[i] = _maxperType;
+        for (uint j = 0; j < timeSlotCount; j++){
+            for (uint i = 0; i < _types; i++) {
+                    epoch_data_all[j].fuZiRemaining[i] = _maxperType;
+                }
+            }
         }
-    }
     
     function getTimeGame(uint256 _timestamp) private pure returns (uint8) {
         uint256 secondsSinceGiveawayStart = _timestamp - START_DATE;
@@ -62,9 +55,9 @@ contract collectFuzi{
     }
     
     function clickFuzi(uint index) returns(bool){
+        uint8 currentTimeSlot = getTimeGame(now);
         require (index <= types);
-        require(epoch_data.fuZiRemaining[index] >= 1);
-        uint8 currentTimeSlot = getTimeSlot(now);
+        require(epoch_data_all[currentTimeSlot].fuZiRemaining[index] >= 1);
         epoch_data_all[currentTimeSlot].fuZiRemaining[index] -= 1;
         epoch_data_all[currentTimeSlot].fuZiof[msg.sender][index] += 1;
         recieveFuZi(index);
@@ -72,18 +65,18 @@ contract collectFuzi{
     }
     
     function getFuZiof(uint index) constant returns(uint){
-        uint8 currentTimeSlot = getTimeSlot(now);
+        uint8 currentTimeSlot = getTimeGame(now);
         return(epoch_data_all[currentTimeSlot].fuZiof[msg.sender][index]);
     }
     
     function getRemaining(uint index) constant returns(uint){
-        uint8 currentTimeSlot = getTimeSlot(now);
+        uint8 currentTimeSlot = getTimeGame(now);
         return epoch_data_all[currentTimeSlot].fuZiRemaining[index];
     }
     
     function checkRedeem() private returns(uint){
         uint count = 0;
-        uint8 currentTimeSlot = getTimeSlot(now);
+        uint8 currentTimeSlot = getTimeGame(now);
         for (uint i=0;i <= types;i++){
             if(epoch_data_all[currentTimeSlot].fuZiof[msg.sender][i]>=1){
                 count +=1;
@@ -97,7 +90,7 @@ contract collectFuzi{
     }
     
     function afterRedeem() private{
-        uint8 currentTimeSlot = getTimeSlot(now);
+        uint8 currentTimeSlot = getTimeGame(now);
         for(uint i =0;i<types;i++){
             epoch_data_all[currentTimeSlot].fuZiof[msg.sender][i] -= 1;
         }
